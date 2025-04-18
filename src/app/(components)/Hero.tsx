b3c09ea3164960400615393bdd1711c1f047ffbb
EldritchGriffin <abdelhak.form@gmail.com>
2025-04-18 17:34:19 +0100

    Fix the Hero component for mobile

--- src/app/(components)/Hero.tsx
+++ src/app/(components)/Hero.tsx
@@ -5,12 +5,12 @@ export default async function Hero() {
   const posts = await getAllPosts()
   return (
     <>
-      <div className="w-full max-w-screen-xl bb">
+      <div className="w-full max-w-screen-xl">
 
-        <section className="container mx-auto">
+        <section className="container mx-auto mb-10">
           <h2 className="text-xl font-bold mb-4">Main Stories</h2>
-          <div className="flex flex-col lg:flex-row gap-2">
-            <article className="lg:w-[60%] relative h-[450px] bg-black text-white flex flex-col justify-end">
+          <div className="flex flex-col lg:flex-row gap-2 bb ">
+            <article className="lg:w-[60%] relative h-[450px]   text-white ">
               <CardPost
                 title={posts.data[0].title}
                 imageUrl={process.env.NEXT_PUBLIC_STRAPI_URL + posts.data[0].banner.url}
@@ -20,9 +20,9 @@ export default async function Hero() {
               ></CardPost>
             </article>
             {/* Secondary stories taking remaining 40% width */}
-            <div className="lg:w-2/5 grid grid-cols-2 gap-2 h-[450px]">
+            <div className="lg:w-2/5 grid grid-cols-1 md:grid-cols-2 gap-2 ">
               {[1, 2, 3, 4].map((_, i) => (
-                <article key={i} className="bg-black">
+                <article key={i} className="  gap-y-2 relative h-[221px]">
                   <CardPost
                     title={posts.data[0].title}
                     imageUrl={process.env.NEXT_PUBLIC_STRAPI_URL + posts.data[0].banner.url}
@@ -37,9 +37,9 @@ export default async function Hero() {
         </section>
         <section className="container mx-auto">
           <h2 className="text-xl font-bold mb-4">Main Stories</h2>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-2 ">
             {[...Array(4)].map((_, i) => (
-              <article key={i} className="aspect-square bg-black shadow flex flex-col justify-end">
+              <article key={i} className="relative h-[301px]">
                 <CardPost
                   title={posts.data[0].title}
                   imageUrl={process.env.NEXT_PUBLIC_STRAPI_URL + posts.data[0].banner.url}
