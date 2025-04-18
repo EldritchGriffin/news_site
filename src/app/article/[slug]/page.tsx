3ea816f2fb901bd708e03996bfad6d625ae9dfd1
EldritchGriffin <abdelhak.form@gmail.com>
2025-04-18 20:20:52 +0100

    feat(added dynamic route)

---
+++ src/app/article/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Markdown from 'react-markdown'
+export default async function page() {
+    return (
+        <main className="bg-white text-gray-900 flex flex-col items-center w-full">
+            <Markdown>
+            </Markdown>
+        </main>
+    )
+}
