aad25ada47c564dbe9d8faff1703f149de819a20
ismail el bakkouchi <ismail.elbakkouchi0@gmail.com>
2025-04-19 19:15:25 +0000

    added focus out event

--- src/app/navbar/navbar-components/media.tsx
+++ src/app/navbar/navbar-components/media.tsx
@@ -6,16 +6,16 @@ import { FaInstagram } from "react-icons/fa6";
 export default function Media () {
     return (
         <div className="flex gap-2 w-fit">
-            <a className="bg-[#222222] border-white rounded-lg px-2.5 content-center py-2.5 text-white">
+            <a className="bg-[#222222] border-white rounded-lg px-2.5 content-center py-2.5 cursor-pointer text-white">
                 <FaFacebookF size={16}/>
             </a>
-            <a className="bg-[#222222] rounded-lg border-white px-2.5 content-center py-2.5 text-white">
+            <a className="bg-[#222222] rounded-lg border-white px-2.5 content-center py-2.5 cursor-pointer text-white">
                 <RiTwitterXFill size={16}/>
             </a>
-            <a className="bg-[#222222] rounded-lg px-2.5 border-white content-center  py-2.5 text-white">
+            <a className="bg-[#222222] rounded-lg px-2.5 border-white content-center cursor-pointer py-2.5 text-white">
                 <FaGooglePlusG size={20}/>
             </a>
-            <a className="bg-[#222222] rounded-lg px-2.5 content-center border-white  py-2.5 text-white">
+            <a className="bg-[#222222] rounded-lg px-2.5 content-center border-white cursor-pointer py-2.5 text-white">
                 <FaInstagram size={16}/>
             </a>
         </div>
