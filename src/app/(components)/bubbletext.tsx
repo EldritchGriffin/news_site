b6b10e07b078e7785e47a1735028c08cfd2188eb
scayho <scayhosama@gmail.com>
2025-04-19 15:23:03 +0100

    nice protection

--- src/app/(components)/bubbletext.tsx
+++ src/app/(components)/bubbletext.tsx
@@ -4,13 +4,16 @@ import React, { useEffect } from 'react';
 
 type Props = {
   _text: string;
-  _width: number;
+  _width: string;
 };
 
 export default function Bubbletext({ _text, _width }: Props) {
   return (
-    <h2 className={`pp w-fit`}>
-      {_text}
-    </h2>
+    <div className={`w-[${_width}] border-2`}>
+
+        <h2 className={`pp leading-[19.2px] font-normal text-[16px] text-center`}>
+          {_text}
+        </h2>
+    </div>
   );
 }
--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,11 +1,13 @@
 // 'use client'
 
 import Bubbletext from "../(components)/bubbletext";
+import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronRight } from 'react-icons/fi';
 
 export default async function Page({ params, }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return(
-    <main className="w-full bg-amber-300">
+    <main className="w-full">
 
       <div className="flex gap-1 text-[16px] font-normal text-[#6c757d] ">
             <span  className="text-[#d42a23]">
@@ -18,13 +20,20 @@ export default async function Page({ params, }: { params: Promise<{ id: string }
               {id}
             </span>
       </div>
-      <div className="relative inline-block bg-amber-900 w-full">
-        <div className="w-full border-2 flex flex-col gap-4">
-          <Bubbletext _text={"JAHAD TRUE KINGjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj"} _width={145}/>
-          <Bubbletext _text={"I ALONE SHALL STAND AND FALL"} _width={500}/>
+      <div className="relative  w-full flex justify-between ">
+        <div className="mb-2">
+
+          <Bubbletext _text={id} _width={"135px"}/>
         </div>
-  {/* Triangle (tail) */}
-  {/* <div className="absolute left-1/2 bottom-0 translate-y-full -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-blue-500"></div> */}
+
+          <div className="flex flex-row justify-end items-end gap-2  border-amber-800 w-40">
+            <div className=" w-fit">
+            <FiChevronLeft  className=" w-[30px] h-[30px] border-2 border-[#cfcfcf]" />
+            </div>
+            <div className=" w-fit">
+            <FiChevronRight  className=" w-[30px] h-[30px] border-2 border-[#cfcfcf]" />
+            </div>
+          </div>
 </div>
 
     
