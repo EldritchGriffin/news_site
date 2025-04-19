01d00baca20456ebd1b367b80c7c15e04e681401
EldritchGriffin <abdelhak.form@gmail.com>
2025-04-19 03:10:40 +0100

    fix blur

---
+++ src/app/(components)/bubble.tsx
@@ -0,0 +1,34 @@
+// 'use client'
+
+// import React, { useEffect } from 'react';
+
+// type Props = {
+//   _text: string;
+//   _width: number;
+// };
+
+// export default function Bubbletext({ _text, _width }: Props) {
+//   return (
+//     <h2 className={`pp w-fit leading-[19.2px] font-normal text-[16px]`}>
+//       {_text}
+//     </h2>
+//   );
+// }
+
+'use client'
+
+import React, { useEffect } from 'react';
+
+type Props = {
+  _text: string;
+  _width: string;
+};
+
+export default function Bubbletext({ _text, _width }: Props) {
+  // alert(`w-[${_width}] border-2`);
+  return (
+    <div className={`pp w-[${_width}] leading-[19.2px] font-normal text-[16px] text-center border-2`}>
+          {_text}
+    </div>
+  );
+}
--- src/app/navbar/navbar.tsx
+++ src/app/navbar/navbar.tsx
@@ -101,7 +101,7 @@ export default function Navbar() {
             title : "title 3",
             content : "this is the content of the page .",
             createdAt : new Date("2025-04-16T20:38:52.530Z"),
-            updatedAt : new Date("2025-04-17T22:41:24.059Z"),
+            updatedAt : new Date("2025-04-17T22:4`1:24.059Z"),
             publishedAt : new Date("025-04-17T22:41:24.066Z"),
             Category : "National",
             banner : "/protest.jpg"
@@ -109,96 +109,95 @@ export default function Navbar() {
     ]
     return (
         <>
-            <header className="w-full h-full flex flex-col scroll-auto">
-                <div className="w-full bg-[#222222] flex justify-center py-3">
-                        <Image
-                          src="/logo-trans.png"
-                          width={288}
-                          height={58}
-                          alt="Logo"
-                        />
-                </div>
-                <div className="w-full bg-red-500">
-                    <button className="text-white flex justify-center pl-2 px-[4px] py-[12px] cursor-pointer"
-                        onClick={() => SetShowList(true)}>
-                        <LuMenu size={30}/>
-                    </button>
-                </div>
-            </header>
-            {ShowList && <section className="max-w-[400px] modal-backdrop h-full inset-0 bg-opacity-30 backdrop-blur-sm py-8">
-                <div className="py-5 px-5">
-                    <button className="flex justify-center align-items-center px-5 text-sm py-1 rounded-sm bg-[#212529] cursor-pointer text-white gap-1"
-                        onClick={() => SetShowList(false)}>
-                        <p>Close</p>
-                        <FaLongArrowAltRight size={15} />
-                    </button>
-                </div>
-                <div className="p-4 flex flex-col gap-3">
-                    <Image
-                        className="pb-1"
-                        src="/mobile-logo.png"
-                        width={152}
-                        height={31}
-                        alt="Logo"
-                    />
-                    <div className="flex gap-2 w-full">
-                        <input placeholder="Search"
-                            className="border w-full text-gray-500 border-gray-500 rounded-sm p-2">
-                        </input>
-                        <button className="bg-white text-green-500 border-1 border-green-500 rounded-sm p-2">
-                            Search
-                        </button>
-                    </div>
-                    <nav>
-                        <ul>
-                            <li  className=" bg-[#f7f7f7] p-3 text-md mb-1 pl-2" >Home</li>
-                            {
-                                ListItems && (
-                                ListItems.map( (item: ListItem, itemIndex: number) => {
-                                    return (
-                                        <div key={itemIndex}>
-                                            <li key={itemIndex} className="flex cursor-pointer gap-2 mb-1 bg-[#f7f7f7] text-md w-full p-3 pl-2 content-center"
-                                                onClick={()=> {
-                                                    if (showNational === itemIndex)
-                                                        setShowNational(-1);
-                                                    else
-                                                        setShowNational(itemIndex);
-                                                    }}>
-                                                <p>
-                                                    {item.name}
-                                                </p>
-                                                <div className="text-sm flex items-center">
-                                                    <TiArrowSortedDown />
-                                                </div>
-                                                </li>
-                                                {showNational == itemIndex && 
-                                                <ul className="overflow-x-auto flex items-scroll">
-                                                    {true && data.map((element: Item, index: number)=> {
-                                                        return (
-                                                            <li key={index} className="bg-[#f7f7f7]">
-                                                                <Item   title={element.title}
-                                                                        content={element.content}
-                                                                        banner={element.banner}
-                                                                        Category={element.Category}>
-                                                                </Item>
-                                                            </li>
-                                                        )
-                                                    })}
-                                                </ul>
-                                            }
-                                        </div>
-                                        )
-                                    })
-                                )
-                            }
-                            <li className=" bg-[#f7f7f7] w-full p-3 text-md mb-1 pl-2" >Blog</li>
-                            <li className=" bg-[#f7f7f7] w-full p-3 text-md mb-1 pl-2" >Contact us</li>
+         <header className="w-full h-full flex flex-col scroll-auto">
+        <div className="w-full bg-[#222222] flex justify-center py-3">
+          <Image src="/logo-trans.png" width={288} height={58} alt="Logo" />
+        </div>
+        <div className="w-full bg-red-500">
+          <button
+            className="text-white flex justify-center pl-2 px-[4px] py-[12px] cursor-pointer"
+            onClick={() => SetShowList(true)}
+          >
+            <LuMenu size={30} />
+          </button>
+        </div>
+      </header>
+      {ShowList && (
+        <section className="fixed inset-0  backdrop-blur-sm z-50">
+          <div className="max-w-[400px] h-full py-8 bg-white">
+            <div className="py-5 px-5">
+              <button
+                className="flex justify-center align-items-center px-5 text-sm py-1 rounded-sm bg-[#212529] cursor-pointer text-white gap-1"
+                onClick={() => SetShowList(false)}
+              >
+                <p>Close</p>
+                <FaLongArrowAltRight size={15} />
+              </button>
+            </div>
+            <div className="p-4 flex flex-col gap-3">
+              <Image
+                className="pb-1"
+                src="/mobile-logo.png"
+                width={152}
+                height={31}
+                alt="Logo"
+              />
+              <div className="flex gap-2 w-full">
+                <input
+                  placeholder="Search"
+                  className="border w-full text-gray-500 border-gray-500 rounded-sm p-2"
+                />
+                <button className="bg-white text-green-500 border-1 border-green-500 rounded-sm p-2">
+                  Search
+                </button>
+              </div>
+              <nav>
+                <ul>
+                  <li className="bg-[#f7f7f7] p-3 text-md mb-1 pl-2">Home</li>
+                  {ListItems.map((item: ListItem, itemIndex: number) => (
+                    <div key={itemIndex}>
+                      <li
+                        className="flex cursor-pointer gap-2 mb-1 bg-[#f7f7f7] text-md w-full p-3 pl-2 content-center"
+                        onClick={() =>
+                          setShowNational(
+                            showNational === itemIndex ? -1 : itemIndex
+                          )
+                        }
+                      >
+                        <p>{item.name}</p>
+                        <div className="text-sm flex items-center">
+                          <TiArrowSortedDown />
+                        </div>
+                      </li>
+                      {showNational === itemIndex && (
+                        <ul className="overflow-x-auto flex items-scroll">
+                          {data.map((element: Item, index: number) => (
+                            <li key={index} className="bg-[#f7f7f7]">
+                              <Item
+                                title={element.title}
+                                content={element.content}
+                                banner={element.banner}
+                                Category={element.Category}
+                              />
+                            </li>
+                          ))}
                         </ul>
-                    </nav>
-                    <Media/>
-                </div>
-            </section>
-            }
+                      )}
+                    </div>
+                  ))}
+                  <li className="bg-[#f7f7f7] w-full p-3 text-md mb-1 pl-2">
+                    Blog
+                  </li>
+                  <li className="bg-[#f7f7f7] w-full p-3 text-md mb-1 pl-2">
+                    Contact us
+                  </li>
+                </ul>
+              </nav>
+              <Media />
+            </div>
+          </div>
+        </section>
+      )}
         </>
     )  
 }  
