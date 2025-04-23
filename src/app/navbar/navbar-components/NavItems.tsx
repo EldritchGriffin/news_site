56ae6a7281c3a961e1e20ebf2f38304855aef4c2
EldritchGriffin <abdelhak.form@gmail.com>
2025-04-23 03:04:42 +0100

    updated text size for title and content

--- src/app/navbar/navbar-components/NavItems.tsx
+++ src/app/navbar/navbar-components/NavItems.tsx
@@ -14,8 +14,8 @@ export default function NavItems(props : {title: string, content: string, banner
               <div className="flex flex-col w-full pl-2 ">
                 <img src={props.banner} alt="Banner" className="w-full h-32 object-cover pb-2 pt-2" />
                 <span className="text-xs p-2 text-white bg-[#d42a23] w-fit">{props.Category}</span>
-                <h2 className="text-lg text-black font-bold">{styleText(props.title)}</h2>
-                <p className="text-sm text-black">{styleText(props.content)}</p>
+                <h2 className="text-lg text-black font-bold">{styleText(props.title, 40)}</h2>
+                <p className="text-sm text-black">{styleText(props.content, 100)}</p>
               </div>
     </div>
   )
