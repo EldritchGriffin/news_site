fb2baea5540bf2ae7cccbb6e87f0c53cb2e58df2
EldritchGriffin <abdelhak.form@gmail.com>
2025-04-23 03:22:31 +0100

    updated css

--- src/app/navbar/navbar-components/NavItems.tsx
+++ src/app/navbar/navbar-components/NavItems.tsx
@@ -14,7 +14,7 @@ export default function NavItems(props : {title: string, content: string, banner
               <div className="flex flex-col w-full pl-2 ">
                 <img src={props.banner} alt="Banner" className="w-full h-32 object-cover pb-2 pt-2" />
                 <span className="text-xs p-2 text-white bg-[#d42a23] w-fit">{props.Category}</span>
-                <h2 className="text-lg text-black mt-4 p-t-4 font-bold">{styleText(props.title, 40)}</h2>
+                <h2 className="text-lg text-black mt-4 p-t-4 font-bold">{styleText(props.title, 100)}</h2>
               </div>
     </div>
   )
