77ca1ae82d1d5deed87e02ecf265fed3d501180d
EldritchGriffin <abdelhak.form@gmail.com>
2025-04-23 03:06:53 +0100

    fixed items structure in navbar

--- src/app/navbar/navbar-components/NavItems.tsx
+++ src/app/navbar/navbar-components/NavItems.tsx
@@ -15,7 +15,6 @@ export default function NavItems(props : {title: string, content: string, banner
                 <img src={props.banner} alt="Banner" className="w-full h-32 object-cover pb-2 pt-2" />
                 <span className="text-xs p-2 text-white bg-[#d42a23] w-fit">{props.Category}</span>
                 <h2 className="text-lg text-black font-bold">{styleText(props.title, 40)}</h2>
-                <p className="text-sm text-black">{styleText(props.content, 100)}</p>
               </div>
     </div>
   )
