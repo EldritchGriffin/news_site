f49a18a2dccdd3fa9974bf2a0a4b22870ebe7dda
EldritchGriffin <abdelhak.form@gmail.com>
2025-04-23 02:49:53 +0100

    added a route

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,13 @@
-import { getLatestPosts } from "../(handlers)/requestHandlers";
+import { getCategoriesFromLast3Days, getLatestPosts } from "../(handlers)/requestHandlers";
 import { getLatestPostsFromCategory } from "../(handlers)/requestHandlers";
 import axios from "axios";
 import { getLatestPostsFromCategoryLast7Days } from "../(handlers)/requestHandlers";
-import {setPublishedAtDateOne10DaysBehind} from "../(handlers)/requestHandlers";
 
 export default async function TestPage() {
-    const posts = await getLatestPostsFromCategoryLast7Days("Política");
-    setPublishedAtDateOne10DaysBehind("atn1l4c1jddk14ozbitw7k79");
-    console.log(posts);
+    const cats = await getCategoriesFromLast3Days();
+    console.log(cats); 
     return (
-    <div>
-    </div>
+        <div>
+        </div>
     );
 }
